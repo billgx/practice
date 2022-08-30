3f7dd5c57438d8f756df15fef0899a02132c8019
billgx <billgx@ksu.edu>
2022-08-30 10:08:45 -0500

    oldMacDonald.js file is for object & git practice

---
+++ oldMacDonald.js
@@ -0,0 +1,86 @@
+/* 
+Title: The Old MacDonald Song
+Date: Aug. 30, 2022
+Objectives: Practice using objects, git & GitHub.
+
+Contributors: 
+Garrett Bennett, Trevor Cunningham, Katie Donovan, Eli Heffernan,
+Tristan Lilly, Dylan Mangels, Bret Miller, Ryan Pfeifer
+Joe Roth, Aidan Sankey, Joya Shelton, Tori Tuggle
+Austin Emberlin, JC Gibbs, Caleb Glen, Daisuke Niisaka
+Prof. Wm. Genereux
+
+In this exercise, we will use JavaScript to create the song
+lyrics to "Old MacDonald. Each person will create a JavaScript
+object with two key/value pairs using "animalName" and "animalSound".
+
+Everyone is to create a unique animal object with it's name and the
+sound that it makes--no duplicates allowed!
+
+For more info on JavaScript Objects see:
+https://www.w3schools.com/jsref/jsref_obj_object.asp
+
+To access the oldMacDonald.js file use git to clone the GitHub repo at:
+https://github.com/billgx/practice.git
+
+*/
+
+
+// Prof. Gx's Example:
+
+const duck = {
+    animalName: "duck",
+    animalSound: "quack!"
+}
+
+// Everyone add your animal object with its 
+// animalName & animalSound below your name:
+
+
+// Garrett
+
+// Trevor
+
+// Katie
+
+// Eli
+
+// Tristan
+
+// Dylan
+
+// Bret
+
+// Ryan
+
+// Joe
+
+// Aidan
+
+// Joya
+
+// Tori
+
+// Austin
+
+// JC Gibbs
+
+// Caleb Glen
+
+// Daisuke
+
+
+/*********************************************
+
+// put the console.log output program below here
+
+const eio = "      E I E I O";
+
+console.log ("Old MacDonald had a farm");
+console.log (eio + "\n");
+
+for (n = 0; n <= 16; n++) {
+    console.log("Testing 123...");
+}
+
+*/
