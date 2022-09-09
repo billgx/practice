6b922faeea77f6df386d59cd5ab2b9b1088c4b7e
eliden09 <112178465+eliden09@users.noreply.github.com>
2022-09-08 22:55:48 -0500

    uploaded code for old mcdonald

---
+++ Old McDonald - Eli Heffernan.js
@@ -0,0 +1,132 @@
+/* 
+Title: The Old MacDonald Song
+Date: Aug. 30, 2022
+Objectives: Practice using objects, git & GitHub.
+Contributors: 
+Garrett Bennett, Trevor Cunningham, Katie Donovan, Eli Heffernan,
+Tristan Lilly, Dylan Mangels, Bret Miller, Ryan Pfeifer
+Joe Roth, Aidan Sankey, Joya Shelton, Tori Tuggle
+Austin Emberlin, JC Gibbs, Caleb Glen, Daisuke Niisaka
+Prof. Wm. Genereux
+In this exercise, we will use JavaScript to create the song
+lyrics to "Old MacDonald. Each person will create a JavaScript
+object with two key/value pairs using "animalName" and "animalSound".
+Everyone is to create a unique animal object with it's name and the
+sound that it makes--no duplicates allowed!
+For more info on JavaScript Objects see:
+https://www.w3schools.com/jsref/jsref_obj_object.asp
+To access the oldMacDonald.js file use git to clone the GitHub repo at:
+https://github.com/billgx/practice.git
+*/
+
+
+// Prof. Gx's Example:
+
+const duck = {
+    animalName: "duck",
+    animalSound: "quack!"
+}
+
+// Everyone add your animal object with its 
+// animalName & animalSound below your name:
+
+
+// Garrett
+
+const wolf = {
+    animalName: "wolf",
+    animalSound: "awoooo!"
+}
+// Trevor
+    const cow = {
+        animalName: "Cow",
+        animalName: "MOO!"
+    }
+
+// Katie
+const pig = {
+    animalName: "pig",
+    animalSound: "oink!"
+}
+
+// Eli
+
+// Tristan
+
+// Dylan
+const cat = {
+	animalName: "cat",
+	animalSound: "meow!"
+}
+
+// Bret
+const sheep = {
+	animalName: "sheep",
+	animalSound: "baa!"
+}
+
+// Ryan
+const crow = {
+	animalName: "crow",
+	animalSound: "caw!"
+}
+
+// Joe
+
+// Aidan
+const tiger ={
+	animalName: "tiger",
+	animalSound: "Roar!"
+}
+// Joya
+
+// Tori
+const frog ={
+	animalName: "frog",
+	animalSound: "ribbit ribbit"
+}
+// Austin
+
+// JC Gibbs
+ const dog = {
+	 animalName: "dog",
+	 animalSound: "woof!"
+ }
+
+// Caleb Glen
+const fox = {
+    animalName: "Fox",
+    animalSound: "ring ding ding ding"
+}
+// Daisuke
+
+
+const animals = [duck, wolf, cow, pig, cat, sheep, crow, tiger, frog, dog, fox];
+/*********************************************/
+
+// put the console.log output program below here
+
+const eio = "      E I E I O";
+
+for(i=0;i<animals.length;i++) {
+
+console.log ("Old MacDonald had a farm");
+
+console.log (eio + "\n");
+
+console.log ("and on his farm he had a " + animals[i].animalName);
+
+console.log (eio + "\n");
+
+
+console.log ("With a " + animals[i].animalSound + " here" );
+
+    console.log ("And a " + animals[i].animalSound + " there" );
+
+    console.log ("Here a " + animals[i].sound + " " + "there a " + animals[i].animalSound);
+
+    console.log ("Everywhere a " + animals[i].animalSound);
+
+    console.log("\n")
+
+}
