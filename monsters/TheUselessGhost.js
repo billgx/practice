87d479585c93d0e3612a14a5cc48977f68584d95
TrevorC55 <112402775+TrevorC55@users.noreply.github.com>
2022-10-27 13:58:08 -0500

    Ghost boy
    Boy of Ghost

---
+++ monsters/TheUselessGhost.js
@@ -0,0 +1,10 @@
+class Ghost {
+    constructor() {
+        this.health = 10;
+        this.damage = 2;
+        this.sound = "boo..."
+        console.log("Some Random person died")
+    }
+}
+    
+let f1 = new Ghost();
