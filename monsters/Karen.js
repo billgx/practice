1ccbe280e91be78abb8fffcc00ede6fc6ca7579d
jroth3 <100368644+jroth3@users.noreply.github.com>
2022-11-01 13:31:21 -0500

    Create Karen.js

---
+++ monsters/Karen.js
@@ -0,0 +1,11 @@
+class Karen {
+    constructor() {
+        this.attack = 100;
+        this.health = 50;
+        this.sound = "How DARE you speak to me that way!";
+        console.log = "Karen has entered the building!"
+        console.log (this.sound);
+    }
+}
+
+let k1 = new Karen();
