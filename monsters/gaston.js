b753751f8de678b45686256fdcdb80ca9b8cc93e
rmpfeifer <112512389+rmpfeifer@users.noreply.github.com>
2022-11-03 13:28:06 -0500

    Create gaston.js

---
+++ monsters/gaston.js
@@ -0,0 +1,10 @@
+class Gaston {
+    constructor() {
+        this.health = 250;
+        this.damage = 20;
+        this.sound = "I eat 5 dozen eggs!"
+        console.log("Gaston the egg eater has spawned")
+    }
+}
+    
+let f1 = new Gaston();
