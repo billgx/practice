d5661ebfef5b79f96558edc65973798358a6fb13
vgtuggle <98615870+vgtuggle@users.noreply.github.com>
2022-11-03 13:45:10 -0500

    Add files via upload

---
+++ monsters/Chucky.js
@@ -0,0 +1,11 @@
+class HauntedDoll {
+  constructor() {
+    this.health = 100;
+    this.sound = "Hi, I'm Chucky, wanna play?!";
+    this.damage = 10;
+    console.log ("We're friends 'til the end, remember?") ;
+    console.log (this.sound);
+  }
+}
+
+let w1 = new HauntedDoll();
