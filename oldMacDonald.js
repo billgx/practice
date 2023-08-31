c573a19081269fcdaf5089d4ace2734de020eea2
Bill Genereux <billgx@ksu.edu>
2023-08-31 13:41:12 -0500

    Reset oldMacDonald.js

--- oldMacDonald.js
+++ oldMacDonald.js
@@ -1,13 +1,10 @@
 /* 
 Title: The Old MacDonald Song
-Date: Aug. 30, 2022
+Date: Aug. 30, 2023
 Objectives: Practice using objects, git & GitHub.
 
 Contributors: 
-Garrett Bennett, Trevor Cunningham, Katie Donovan, Eli Heffernan,
-Tristan Lilly, Dylan Mangels, Bret Miller, Ryan Pfeifer
-Joe Roth, Aidan Sankey, Joya Shelton, Tori Tuggle
-Austin Emberlin, JC Gibbs, Caleb Glen, Daisuke Niisaka
+
 Prof. Wm. Genereux
 
 In this exercise, we will use JavaScript to create the song
@@ -37,99 +34,8 @@ const duck = {
 // animalName & animalSound below your name:
 
 
-// Garrett
 
-const wolf = {
-    animalName: "wolf",
-    animalSound: "awoooo!"
-}
-// Trevor
-    const cow = {
-        animalName: "Cow",
-        animalName: "MOO!"
-    }
-
-// Katie
-const pig = {
-    animalName: "pig",
-    animalSound: "oink!"
-}
-
-// Eli
-const chicken = {
-    animalName: "chicken",
-    animalSound: "bawk bawk!"
-}
-// Tristan
-const GiantSquid ={
-	animalName: "Giant Squid",
-	animalSound: "Squelch"
-}
-
-// Dylan
-const cat1 = {
-	animalName: "cat",
-	animalSound: "meow!"
-
-}
-// Bret
-const sheep = {
-	animalName: "sheep",
-	animalSound: "baa!"
-}
-
-// Ryan
-const crow = {
-	animalName: "crow",
-	animalSound: "caw!"
-}
-
-// Joe
-const mouse = {
-	animalName: "mouse",
-	animalSound: "squeak!"
-}
-
-// Aidan
-const tiger ={
-	animalName: "tiger",
-	animalSound: "Roar!"
-}
-// Joya
-const cat = {
-	 animalName: "cat",
-	 animalSound: "meow!"
- }
-// Tori
-const frog ={
-	animalName: "frog",
-	animalSound: "ribbit ribbit"
-}
-// Austin
-const peacock = {
-	animalName: "peacock",
-	animalSound: "Caaaawww! Caaaawww!"
-}
-
-// JC Gibbs
- const dog = {
-	 animalName: "dog",
-	 animalSound: "woof!"
- }
-
-// Caleb Glen
-const fox = {
-    animalName: "Fox",
-    animalSound: "ring ding ding ding"
-}
-// Daisuke
-const horse = {
-	animalName:"horse",
-	animalSound:"neigh!"
-}
-
-
-const animals = [duck, wolf, cow, pig, fox, dog, frog, cat, cat1, tiger, crow, sheep, chicken, horse, peacock]
+const animals = [duck]
 
 // put the console.log output program below here
 
